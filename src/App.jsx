780f77931f8f4f4890c529b78181445ee48ad41e
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-07-16 11:06:05 +0200

    feat: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Route, Routes } from "react-router-dom"
 import ProjectListPage from "./pages/ProjectListPage"
+import HomePage from "./pages/HomePage"
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <>
-      <h1>React Project Management</h1>
+      <Navbar />
 
-      <ProjectListPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectListPage />} />
+      </Routes>
+
     </>
   )
 }
